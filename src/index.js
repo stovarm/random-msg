f2eccb0b14181bd07b2b2367036dc348952b4655
stovarm <stovarm@gmail.com>
2021-09-20 07:37:16 -0500

    First commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const messages = [
+    "Pedro",
+    "Ana",
+    "Nicolay",
+    "Diego",
+    "Freddy",
+    "Paulina",
+    "Carolina",
+    "Laura"
+];
+
+const randomMsg = () => {
+    const message = messages[Math.floor(Math.random() * messages.length)];
+    console.log(message);
+}
+
+module.exports = { randomMsg };
